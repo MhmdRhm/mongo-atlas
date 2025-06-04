078781ca7c57ffa0a8b4934a727d849eaf623d88
MhmdRhm <mhmdrhm.it@gmail.com>
2025-06-04 17:27:18 +0330

    First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String
+});
+
+const counter = 0;
+
+app.get('/', async (_, res) => {
+    counter++;
+    const User = mongoose.model('User', userSchema);
+
+    const mora = new User({username: "Mohammad", email: "Mhdmrh@gmail.com", password: "kljdalsfjlads"});
+    await mora.save();
+    res.send(`<h1 style="text-align: center">${mora.username}</h1>`);
+});
+
+
+mongoose.connect("mongodb+srv://mhmdrhmit:learncode@cluster0.gs3a1.mongodb.net/testdb?retryWrites=true&w=majority&appName=Cluster0").then(() => {
+    app.listen(3000);
+});
